dd735a62399162088b70af5c85b682ec7f3407d5
Isabella <151046213+isabella123-pinkbadger@users.noreply.github.com>
2024-10-31 12:50:31 -0400

    consequences

--- code.js
+++ code.js
@@ -21,20 +21,45 @@ function startGame() {
 // Function for the attic path
 function attic() {
     // Present the player with a choice
-    let choice = prompt("You see a dusty old board game with a spooky green light flashing. Do you (1) play it or (2) throw it away?");
+    let choice = prompt("You see a dusty old board game with a spooky green light flashing. Do you (1) play it or (2) throw it away or (3) burn it?");
     
     // Handle the player's choice
     if (choice === "1") {
         alert("You get sucked into the game and try to survive!");
     } else if (choice === "2") {
-        alert("Bad Karma for the rest of your life!");
+        alert("You throw the board game at the window but it hits the wall and bounces back, impaling your cranium");
+    } else if (choice === "3") {
+        alert("The ghosts get attracted to the fire and you get slimed!");
     } else {
         alert("Invalid choice! The ghost gets you for not choosing!");
     }
 
     // End the game
-    alert("The End. Thanks for playing!");
+    alert("Thanks for playing, Happy Halloween!");
 }
+
+// Function for the bedroom path
+function bedroom() {
+    // Present the player with a choice
+    let choice = prompt("You hear suspenseful noises. Do you (1) investigate or (2) run away or (3) hide in the closet?");
+    
+    // Handle the player's choice
+    if (choice === "1") {
+        alert("You discover a hidden treasure full of baked goods!");
+    } else if (choice === "2") {
+        alert("You run into a lamp, go unconscious, then wake up in a kidnappers basement!");
+     } else if (choice === "3") {
+        alert("You drop into a pool full of flesh-eating piranhas");
+    } else {
+        alert("Invalid choice! The ghost gets you for not choosing!");
+    }
+
+    // End the game
+    alert("Thanks for playing, Happy Halloween!");
+}
+
+// Start the game
+startGame();
 
 
 
